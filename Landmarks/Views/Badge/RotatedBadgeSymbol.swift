308e0d0078e8115a8457ec243850dbd0f59468a8
Sriket Komali <sriketk5@gmail.com>
2025-12-07 21:53:31 -0500

    badge bs

---
+++ Landmarks/Views/Badge/RotatedBadgeSymbol.swift
@@ -0,0 +1,29 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Landmarks
+//
+//  Created by Sriket Komali on 12/7/25.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+
+    var body: some View {
+
+        BadgeSymbol()
+
+            .padding(-60)
+
+            .rotationEffect(angle, anchor: .bottom)
+
+    }
+
+}
+
+#Preview {
+
+    RotatedBadgeSymbol(angle: Angle(degrees: 5))
+
+}
